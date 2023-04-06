051f4d309405649280118f2c323b88a41e521116
Student <student@iMac-Astrum-93.local>
2023-04-06 18:50:25 +0500

    Going home

--- src/ui/input.jsx
+++ src/ui/input.jsx
@@ -1,17 +1,17 @@
 const Input = ({ label, state, setState, type = "text" }) => {
-  return (
-    <div className="form-floating my-2">
-      <input
-        type={type}
-        className="form-control"
-        // id="floatingInput"
-        placeholder={label}
-        value={state}
-        onChange={(e) => setState(e.target.value)}
-      />
-      <label for="floatingInput">{label}</label>
-    </div>
-  );
+    return (
+        <div className="form-floating my-2">
+            <input
+                type={type}
+                className="form-control"
+                // id="floatingInput"
+                placeholder={label}
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+            />
+            <label htmlFor="floatingInput">{label}</label>
+        </div>
+    );
 };
 
 export default Input;
